aa465f41586707aa2a1cf86ac1846424c9b29b0e
charanhu <charanhumail@gmail.com>
2020-10-14 14:59:59 +0530

    second

---
+++ booleanexpressions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+int x = 10;
+int y = 9;
+cout << (x > y)<<endl; // returns 1 (true), because 10 is higher than 9
+  cout << (x < y)<<endl;
+  cout << (10 > 9)<<endl; // returns 1 (true), because 10 is higher than 9
+ 
+ int z = 10;
+cout << (z == 10)<<endl;  // returns 1 (true), because the value of x is equal to 10 
+cout << (10 == 15);  // returns 0 (false), because 10 is not equal to 15
+return 0;
+}
---
+++ booleans.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+bool CodingFun = true;
+bool FishTasty = false;
+cout << CodingFun<<endl;  // Outputs 1 (true)
+cout << FishTasty;  // Outputs 0 (false)
+  return 0;
+}
