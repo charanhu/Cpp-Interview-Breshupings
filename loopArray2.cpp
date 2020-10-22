f277479a7d0e9746653cb4ac8fae10f6721764ce
charanhu <charanhumail@gmail.com>
2020-10-22 09:10:07 +0530

    arrayOmmit

---
+++ loopArray2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+for(int i = 0; i < 4; i++) {
+  cout << i << ": " << cars[i] << "\n";
+}
+}
