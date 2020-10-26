f20b8a390604b8a78b1c31955553ed9eecb96a1e
charanhu <charanhumail@gmail.com>
2020-10-26 09:17:14 +0530

    pointers

---
+++ pointers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+string food = "Pizza"; // A food variable of type string
+
+cout << food<<endl;  // Outputs the value of food (Pizza)
+cout << &food; // Outputs the memory address of food (0x6dfed4)
+}
