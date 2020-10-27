213a39b61d3bb7b07329ada84f7d256b781b5087
charanhu <charanhumail@gmail.com>
2020-10-27 09:15:57 +0530

    Pizza

--- pointers1.cpp
+++ pointers1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-string food = "Pizza";  // A food variable of type string
+string food = "Pizaza";  // A food variable of type string
 string* ptr = &food;    // A pointer variable, with the name ptr, that stores the address of food
 
 // Output the value of food (Pizza)
